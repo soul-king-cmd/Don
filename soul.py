d27997830ad7f393fd020c9c377e7b4f04cd32ed
soul-king-cmd <soulk9617@gmail.com>
2025-03-04 12:46:39 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -15,10 +15,10 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 loop = asyncio.get_event_loop()
 
 # Bot Configuration: Set with Authority
-TOKEN = 'YOUR TOKEN'
-ADMIN_USER_ID = USER ID YOUR
+TOKEN = '7671259065:AAG-IGtZUOgqi5-FgkerDXkWT8GMzJSWSvs'
+ADMIN_USER_ID = 1379686370
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
-USERNAME = "@VIPMODSXADMIN"  # Immutable username for maximum security
+USERNAME = "@MRbhanuYT"  # Immutable username for maximum security
 
 # Attack Status Variable to Control Single Execution
 attack_in_progress = False
